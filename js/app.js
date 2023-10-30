87deb7eefd1daa28acc97f440800f67a44a94faf
Umidov Temur <84757889+Temur112@users.noreply.github.com>
2023-10-30 10:25:06 +0500

    Add files via upload
    last

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    window["FLS"] = true;
+    isWebp();
+    const blocks = document.querySelectorAll(".candidate-voting");
+    blocks.forEach((block => {
+        const body_part = block.querySelector(".candidate-voting__body");
+        const icon = block.querySelector(".candidate-voting__icon");
+        block.addEventListener("click", (() => {
+            body_part.classList.toggle("active");
+            icon.classList.toggle("rotate");
+        }));
+    }));
+})();
